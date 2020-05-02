2436b9ea35e07555273f3876e4dae628b9959757
Sebastián Ramírez <tiangolo@gmail.com>
2020-05-02 18:42:31 +0200

    :wrench: Update format scripts

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 set -x
 
-autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place pydantic_sqlalchemy tests --exclude=__init__.py
-black pydantic_sqlalchemy tests
-isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --thirdparty pydantic_sqlalchemy --apply pydantic_sqlalchemy tests
+autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place ./ --exclude=__init__.py
+black ./
+isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --thirdparty pydantic_sqlalchemy --apply ./
