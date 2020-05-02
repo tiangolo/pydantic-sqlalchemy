702de3a3d407cd4f930cf87494742bc9bac50721
Sebastián Ramírez <tiangolo@gmail.com>
2020-05-02 18:12:43 +0200

    :wrench: Add scripts

---
+++ scripts/lint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+mypy pydantic_sqlalchemy
+black pydantic_sqlalchemy tests --check
+isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only --thirdparty pydantic_sqlalchemy pydantic_sqlalchemy tests
---
+++ scripts/test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+bash ./scripts/lint.sh
+pytest --cov=pydantic_sqlalchemy --cov=tests --cov-report=term-missing ${@}
