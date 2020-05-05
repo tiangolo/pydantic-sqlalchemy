28c1b1e0f40ef84a53e81d6eb043a4e2dcfa199b
Sebastián Ramírez <tiangolo@gmail.com>
2020-05-05 08:38:22 +0200

    :bug: Use importlib.metadata in Python 3.8

--- pydantic_sqlalchemy/__init__.py
+++ pydantic_sqlalchemy/__init__.py
@@ -1,4 +1,7 @@
-from importlib_metadata import version
+try:  # pragma: nocover
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version
 
 from .main import sqlalchemy_to_pydantic
 
