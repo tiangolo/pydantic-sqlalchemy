2a1a6e3723a211ad5b3f51a6a5636d47b56d2675
Sebastián Ramírez <tiangolo@gmail.com>
2020-08-31 16:46:02 +0200

    🔧 Update formatting scripts

--- scripts/format-imports.sh
+++ scripts/format-imports.sh
@@ -2,5 +2,5 @@
 set -x
 
 # Sort imports one per line, so autoflake can remove unused imports
-isort --recursive  --force-single-line-imports --thirdparty pydantic_sqlalchemy --apply ./
+isort --recursive  --force-single-line-imports --thirdparty pydantic_sqlalchemy --apply ./pydantic_sqlalchemy ./tests
 sh ./scripts/format.sh
