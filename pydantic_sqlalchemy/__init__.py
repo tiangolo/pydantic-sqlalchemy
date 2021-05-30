000a2be80330bda287b41b09d7211f15e8990595
Sebastián Ramírez <tiangolo@gmail.com>
2021-05-30 11:32:50 +0200

    ✨ Add poetry-version-plugin, remove importlib-metadata dependency

--- pydantic_sqlalchemy/__init__.py
+++ pydantic_sqlalchemy/__init__.py
@@ -1,8 +1,3 @@
-try:  # pragma: nocover
-    from importlib.metadata import version  # type: ignore
-except ImportError:
-    from importlib_metadata import version  # type: ignore
-
 from .main import sqlalchemy_to_pydantic
 
-__version__ = version(__package__)
+__version__ = "0.0.9"
