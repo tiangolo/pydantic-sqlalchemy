13ec9035d1c5986ceb3dccd1d50e2ed9d0d71b7f
tetelevm <tetelev99@mail.ru>
2021-07-30 19:39:24 +0300

    Reduced cyclomatic complexity

--- pydantic_sqlalchemy/main.py
+++ pydantic_sqlalchemy/main.py
@@ -15,23 +15,28 @@ def sqlalchemy_to_pydantic(
     mapper = inspect(db_model)
     fields = {}
     for attr in mapper.attrs:
-        if isinstance(attr, ColumnProperty):
-            if attr.columns:
-                name = attr.key
-                if name in exclude:
-                    continue
-                column = attr.columns[0]
-                python_type: Optional[type] = None
-                if hasattr(column.type, "impl"):
-                    if hasattr(column.type.impl, "python_type"):
-                        python_type = column.type.impl.python_type
-                elif hasattr(column.type, "python_type"):
-                    python_type = column.type.python_type
-                assert python_type, f"Could not infer python_type for {column}"
-                default = None
-                if column.default is None and not column.nullable:
-                    default = ...
-                fields[name] = (python_type, default)
+        if not isinstance(attr, ColumnProperty):
+            continue
+        if not attr.columns:
+            continue
+
+        name = attr.key
+        if name in exclude:
+            continue
+
+        column = attr.columns[0]
+        python_type: Optional[type] = None
+        if hasattr(column.type, "impl"):
+            if hasattr(column.type.impl, "python_type"):
+                python_type = column.type.impl.python_type
+        elif hasattr(column.type, "python_type"):
+            python_type = column.type.python_type
+        assert python_type, f"Could not infer python_type for {column}"
+        default = None
+        if column.default is None and not column.nullable:
+            default = ...
+        fields[name] = (python_type, default)
+
     pydantic_model = create_model(
         db_model.__name__, __config__=config, **fields  # type: ignore
     )
