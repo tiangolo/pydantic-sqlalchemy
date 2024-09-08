c20b64c5105aae4668ac872e0bd275d8588683a2
Sebastián Ramírez <tiangolo@gmail.com>
2024-09-08 17:46:16 +0200

    🔧 Re-create Python project config, dependencies, and CI, just to make CI run (#101)

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -e
 set -x
 
-autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place ./pydantic_sqlalchemy ./tests --exclude=__init__.py
-black ./pydantic_sqlalchemy ./tests
-isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --thirdparty pydantic_sqlalchemy --apply ./pydantic_sqlalchemy ./tests
+ruff check pydantic_sqlalchemy tests scripts --fix
+ruff format pydantic_sqlalchemy tests scripts
